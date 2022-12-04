0725fb7d2511d01aa9fcc55290ec3ce63adc05e7
Charlie Francis Cassidy <charliefrancis.cassidy@gmail.com>
2022-12-04 16:16:30 +1100

    2022 - Day 1 - Second puzzle solved
    This puzzle was to find the top 3 "elves" calorie counts, and find the total calories between them.
    
    To do this, I sorted our list of elves (and their calorie counts), then grabbed the last 3 entries, added them up and there was the answer.

--- 2022/day_one_puzzle.py
+++ 2022/day_one_puzzle.py
@@ -45,14 +45,60 @@ def find_most_calories(elves):
     
     return most_calories
 
-def sort_elves(elves):
-    pass
+# Merge Sort algorithm
+def merge(arr, left, mid, right):
+    left_length = mid - left + 1
+    right_length = right - mid
+
+    # temp arrays
+    left_array = [0] * left_length
+    right_array = [0] * right_length
+
+    for i in range(0, left_length):
+        left_array[i] = arr[left + i]
+
+    for j in range(0, right_length):
+        right_array[j] = arr[mid + 1 + j]
+
+    i = 0
+    j = 0
+    k = left
+
+    while i < left_length and j < right_length:
+        if left_array[i] < right_array[j]:
+            arr[k] = left_array[i]
+            i += 1
+        else:
+            arr[k] = right_array[j]
+            j += 1
+        k += 1
+
+    while i < left_length:
+        arr[k] = left_array[i]
+        i += 1
+        k += 1
+
+    while j < right_length:
+        arr[k] = right_array[j]
+        j += 1
+        k += 1
+
+def merge_sort(arr, left, right):
+    if left < right:
+        mid = left + (right - left) // 2
+
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid + 1, right)
+        merge(arr, left, mid, right)
 
 def top_elves_most_calories(elves, top_count):
     top_elves = []
 
-    for elf in elves:
-        pass
+    for i in range(0, top_count):
+        if i >= len(elves):
+            break
+
+        top_elves.append(elves[-(i + 1)])
 
     return top_elves
 
@@ -69,11 +115,26 @@ def display_answer():
     ex_elves_calories = get_elves_calories_each(example_input_list)
     ex_most_calories = find_most_calories(ex_elves_calories)   
     print("Example - Most Calories:", ex_most_calories)
+
+    ex_elves_len = len(ex_elves_calories)
+    merge_sort(ex_elves_calories, 0, ex_elves_len - 1)
+    ex_top_elves = top_elves_most_calories(ex_elves_calories, 3)
+    ex_top_elves_total_cals = get_total_calories(ex_top_elves)
+    print("Example - Sorted Elves:", ex_elves_calories)
+    print("Example - Top Elves", ex_top_elves)
+    print("Example - Top Elves Total Calories:", ex_top_elves_total_cals)
     
     
     elves_calories = get_elves_calories_each(input_list)
     most_calories = find_most_calories(elves_calories)
     print("Answer - Most Calories:", most_calories)
+
+    elves_len = len(elves_calories)
+    merge_sort(elves_calories, 0, elves_len - 1)
+    top_elves = top_elves_most_calories(elves_calories, 3)
+    top_elves_total_cals = get_total_calories(top_elves)
+    print("Example - Top Elves", top_elves)
+    print("Example - Top Elves Total Calories:", top_elves_total_cals)
 
 parse_files()
 display_answer()
