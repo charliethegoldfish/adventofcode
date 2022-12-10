c141bbbd744aa03cb61485c47f16855550ad1de8
Charlie Francis Cassidy <charliefrancis.cassidy@gmail.com>
2022-12-10 15:19:52 +1100

    2022 - Day 3 - Second half solved
    - Divided rucksacks into groups of 3
    - Looped through all items in first rucksack and compared to other rucksacks
    - If item is in all of them, return that item
    - Re-used function that calculates priority counts to return the answer

--- 2022/day_three_puzzle.py
+++ 2022/day_three_puzzle.py
@@ -13,6 +13,8 @@ example_input_list = []
 # A - Z = 27 - 52
 priority_string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+rucksacks_per_group = 3
+
 def parse_files():
     utils.parse_file(example_input_fname, example_input_list)
     utils.parse_file(input_fname, input_list)
@@ -26,10 +28,42 @@ def fill_rucksack_compartments(input):
     return rucksacks
 
 def group_rucksacks(input):
-    pass
+    grouped_rucksacks = []
+    current_group = []
+    current_count = 0;
+    
+    for rucksack in input:
+        current_group.append(rucksack)
+        current_count += 1
+
+        if current_count % rucksacks_per_group == 0:
+            grouped_rucksacks.append(current_group)
+            current_group = []
+
+    return grouped_rucksacks
+
+def is_item_in_rucksack(item, rucksack):
+    return rucksack.find(item) != -1
 
 def find_item_in_group(rucksacks):
-    pass
+    num_in_group = len(rucksacks)
+
+    for item in rucksacks[0]:
+        found = True
+        for index in range(1, num_in_group):
+            if not is_item_in_rucksack(item, rucksacks[index]):
+                found = False
+        if found:
+            return item
+
+def find_group_items(rucksacks):
+    items = []
+
+    for group in rucksacks:
+        item = find_item_in_group(group)
+        items.append(item)
+    
+    return items
 
 def find_item_in_both_compartments(rucksack):
     for item in rucksack[0]:
@@ -53,6 +87,7 @@ def get_total_priorities(items):
 
 
 def display_answers():
+    # Part 1
     # print("Example - Input list:", example_input_list)
     example_rucksacks = fill_rucksack_compartments(example_input_list)
     # print("Example - Rucksacks:", example_rucksacks)
@@ -61,11 +96,27 @@ def display_answers():
     ex_total_priorities = get_total_priorities(ex_items)
     print("Example - Total priorities:", ex_total_priorities)
 
+    # Part 2
+    ex_grouped_rucksacks = group_rucksacks(example_input_list)
+    # print("Example - Grouped rucksacks:", ex_grouped_rucksacks)
+    ex_group_items = find_group_items(ex_grouped_rucksacks)
+    print("Example - Found group items:", ex_group_items)
+    ex_group_priorities = get_total_priorities(ex_group_items)
+    print("Example - Total group priorities:", ex_group_priorities)
+
+    # Part 1
     rucksacks = fill_rucksack_compartments(input_list)
     items = found_items(rucksacks)
-    print("Answer Found items:", items)
+    # print("Answer Found items:", items)
     total_priorities = get_total_priorities(items)
     print("Answer - Total Priorities", total_priorities)
+
+    # Part 2
+    grouped_rucksacks = group_rucksacks(input_list)
+    group_items = find_group_items(grouped_rucksacks)
+    total_group_priorities = get_total_priorities(group_items)
+    print("Answer - Total Group Priorities", total_group_priorities)
+
 
 parse_files()
 display_answers()
